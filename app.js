6afaa6bacb2e4022cbf725beb0517a9fa04f3b77
Beatriz Marques da Silva <beatricemarx@outlook.com>
2025-08-15 19:14:27 -0300

    Adicionar Amigos na lista para o sorteio

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function adicionarAmigo() {
+    //função para adicionar Amigos na lista para sorteio
+
+    listaAmigos = document.querySelector("input").value;
+    amigos.push(listaAmigos)
+    console.log(amigos)
+    return listaAmigos
+}
+
+
+//console.log(amigos);
